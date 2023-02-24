eedd55e39c16e3e5776e6b0e40d563d7d8307e4d
KhoaVH <huykhoa630@gmail.com>
2023-02-24 16:30:20 +0700

    create custom component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./CustomButton.css";
+
+export function Button(props: any) {
+  const { children, onClick, disabled } = props;
+
+  return (
+    <button className="custom-button" onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onClick: () => {},
+  disabled: false,
+};
+
+{
+  /* <CustomButton onClick={() => console.log('Button clicked')}>
+Click Me
+</CustomButton> */
+}
