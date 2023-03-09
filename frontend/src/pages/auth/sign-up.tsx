e6262e77938ee4b31e8b53bb60d80dfc6b84a0e1
khoaVH <huykhoa630@gmail.com>
2023-03-09 21:35:01 +0700

    khoavh/create layout auth

--- frontend/src/pages/auth/sign-up.tsx
+++ frontend/src/pages/auth/sign-up.tsx
@@ -1,14 +1,19 @@
-import { Button, Checkbox, Input, Typography } from "@material-tailwind/react";
+import { Button, Input, Typography } from "@material-tailwind/react";
+import { NavLink } from "react-router-dom";
 
 export function SignUp() {
   return (
-    <section className="w-96 flex flex-col gap-4">
+    <section className="bg-white w-96 flex flex-col gap-4 p-10 rounded-lg">
       <h1 className="text-center">
         <Typography variant="h4" color="blue-gray">
           Sign Up
         </Typography>
       </h1>
       <form className="flex flex-col gap-4" action="">
+        <div className="flex flex-col gap-1">
+          <Typography>Email</Typography>
+          <Input name="email" label="Email" />
+        </div>
         <div className="flex flex-col gap-1">
           <Typography>User Name</Typography>
           <Input name="username" label="Username" />
@@ -17,31 +22,22 @@ export function SignUp() {
           <Typography>Password</Typography>
           <Input name="password" label="Password" />
         </div>
-        <Button color="blue">Sign In</Button>
-        <div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center "
-              >
-                I agree the
-                <a href="#" className="font-medium transition-colors">
-                  &nbsp;Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
+        <Button color="blue">Create an account</Button>
+        <div className="text-center">
+          <Typography>Or sign up with</Typography>
         </div>
-        <Button variant="gradient">Continue with Facebook</Button>
+        <Button className="h-10" variant="gradient">
+          Continue with Facebook
+        </Button>
         <Button color="light-blue">Continue with Twitter</Button>
         <Typography color="gray" className="mt-4 text-center">
-          Already have an account?{" "}
-          <a href="#" className="font-medium text-blue-500 transition-colors ">
+          Already have an account?
+          <NavLink
+            to="/auth/sign-in"
+            className="font-medium text-blue-500 transition-colors "
+          >
             Sign In
-          </a>
+          </NavLink>
         </Typography>
       </form>
     </section>
