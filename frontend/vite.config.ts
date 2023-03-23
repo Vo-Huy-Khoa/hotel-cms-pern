11ba43cc86268e5824920a3e7afb3edc11500bd9
KhoaVH <huykhoa630@gmail.com>
2023-03-23 10:54:14 +0700

    fix config vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import historyFallback from "connect-history-api-fallback";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    middleware: [historyFallback()],
+  },
+});
